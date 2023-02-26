024051f2e645840bb3a936f4b5edd04c7686be7e
Davidcg182 <davidcasti182@gmail.com>
2023-02-26 11:10:08 -0500

    intento url dog para visuzlizacion de post

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,21 +6,21 @@ import Link from 'next/link';
 
 const name = 'Dave';
 export const siteTitle = 'Next.js Sample Website';
+console.log(encodeURI(siteTitle))
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/profile.jpg"/>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://s1.eestatic.com/2020/10/19/curiosidades/mascotas/mascotas-alimentacion-perros_529458667_162988306_1706x960.jpg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
